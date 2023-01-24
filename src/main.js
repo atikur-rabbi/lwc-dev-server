709dda2765d3ee3d6c145761997fd321117f7267
atikur-rabbi <msawon94@gmail.com>
2023-01-24 14:40:00 +0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import App from "c/app";
+import Header from "c/header";
+import Theme from "c/theme";
+
+
+customElements.define('c-app', App.CustomElementConstructor);
+// customElements.define('c-header', Header.CustomElementConstructor);
+customElements.define('c-theme', Theme.CustomElementConstructor);
